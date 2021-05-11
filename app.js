4009d09308672352a46d13f043df89c6795849b1
Xila Luna <xilaluna2@gmail.com>
2021-05-11 15:59:36 -0700

    [add] deployment configuration

--- app.js
+++ app.js
@@ -43,6 +43,7 @@ app.get("/greetings/:name", (req, res) => {
 })
 
 // Start Server
-app.listen(3000, () => {
-  console.log("Gif Search listening on port localhost:3000!")
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log("Gif Search Running")
 })
